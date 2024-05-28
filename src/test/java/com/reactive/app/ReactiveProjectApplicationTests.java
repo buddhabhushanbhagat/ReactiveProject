a231ca92c7904e12f17c5c05475c4bffedadb78b
bhushan <bhagatbuddhabhushan@gmail.com>
2024-05-28 16:33:09 +0530

    Created new reactive project

---
+++ src/test/java/com/reactive/app/ReactiveProjectApplicationTests.java
@@ -0,0 +1,17 @@
+package com.reactive.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+	@Test
+	void workingWithMono() {
+		System.out.println("testing");
+	}
+
+}
